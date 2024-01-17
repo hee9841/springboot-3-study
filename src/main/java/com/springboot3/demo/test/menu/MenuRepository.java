7d48a933a3d964a21d19637501315d6b9e8f4839
hee9841 <lee98417716@gmail.com>
2024-01-17 20:03:38 +0900

    refactor: 프로젝트를 위한 이전 공부 코드 삭제
    - Member, MemberRepository, MemberService, TestController
    - Menu,
    MenuRepository, MenuService
    - 추가했던 테스트 코드들
    - data.sql

--- src/main/java/com/springboot3/demo/test/menu/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot3.demo.test.menu;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MenuRepository extends JpaRepository<Menu, Long> {
-}
--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO member (id, name) VALUES (1, 'name1')
-INSERT INTO member (id, name) VALUES (2, 'name2')
-INSERT INTO member (id, name) VALUES (3, 'name3')
-
--- INSERT INTO menu (name, price) VALUES ('에스프레스', 1000)
