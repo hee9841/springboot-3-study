e9ed79b37307063f7b52a1c21e32f4de9b61a6b3
hee9841 <lee98417716@gmail.com>
2024-01-18 18:43:23 +0900

    feat: 블로그 글 crud view 관련 기능 추가

--- src/main/java/com/springboot3/demo/controller/BlogViewController.java
+++ src/main/java/com/springboot3/demo/controller/BlogViewController.java
@@ -1,12 +1,17 @@
 package com.springboot3.demo.controller;
 
+import com.springboot3.demo.domain.Article;
 import com.springboot3.demo.dto.ArticleListViewResponse;
+import com.springboot3.demo.dto.ArticleViewResponse;
 import com.springboot3.demo.service.BlogService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -24,5 +29,27 @@ public class BlogViewController {
 
         return "articleList";
 
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
+
+    }
+
+    @GetMapping("/new-article")
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+
+        return "newArticle";
     }
 }
---
+++ src/main/java/com/springboot3/demo/dto/ArticleViewResponse.java
@@ -0,0 +1,22 @@
+package com.springboot3.demo.dto;
+
+import com.springboot3.demo.domain.Article;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ArticleViewResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+
+    public ArticleViewResponse(Article article) {
+        this.id = article.getId();
+        this.title = article.getTitle();
+        this.content = article.getContent();
+        this.createdAt = article.getCreatedAt();
+    }
+}
