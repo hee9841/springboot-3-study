9a42e1078f31a687e00c08dfceb81295d52946ed
hee9841 <lee98417716@gmail.com>
2024-01-18 17:50:26 +0900

    feat: 블로그 글 create, update 시간 컬럼 추가

--- src/main/java/com/springboot3/demo/domain/Article.java
+++ src/main/java/com/springboot3/demo/domain/Article.java
@@ -5,10 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 @Entity
 @Getter
@@ -26,9 +29,17 @@ public class Article {
     @Column(name = "content", nullable = false)
     private String content;
 
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
 //    @Column(name = "writer", nullable = false)
 //    private String writer;
-    
+
     @Builder
     public Article(String title, String content) {
         this.title = title;
